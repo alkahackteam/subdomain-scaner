f24f910282de8165cc1db785b22827f9d415179b
alkahackteam <120032784+alkahackteam@users.noreply.github.com>
2023-02-02 12:08:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import socket
+
+def check_host(host, port):
+    try:
+        sock = socket.create_connection((host, port), timeout=5)
+        return True
+    except:
+        return False
+
+def check_subdomains(domain, wordlist):
+    with open(wordlist, "r") as subdomains:
+        for subdomain in subdomains:
+            subdomain = subdomain.strip() + "." + domain
+            if check_host(subdomain, 80):
+                print("[X] Bulunan subdomain:", subdomain)
+
+print("""
+	
+
+                             _      ___ _           
+  /\  |  |/  /\    |_|  /\  /  |/    | |_  /\  |\/| 
+ /--\ |_ |\ /--\   | | /--\ \_ |\    | |_ /--\ |  | 
+____________________________________________________ 
+ coder: X_User
+""")
+
+target_domain = input("Hedef site: ")
+wordlist = "wordlist.txt"
+
+check_subdomains(target_domain, wordlist)
